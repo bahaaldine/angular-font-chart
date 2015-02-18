7e222cf28599064d526f192f5f6d9137cbcbfd17
Bahaaldine Azarmi <bahaaldine@gmail.com>
2015-02-18 09:54:27 +0100

    removed odometer styling

--- dist/angular-font-chart.js
+++ dist/angular-font-chart.js
@@ -1,5 +1,5 @@
-/*! angular-font-chart - v0.9.16 - 2014-07-12
-* Copyright (c) 2014 ; Licensed  */
+/*! angular-font-chart - v0.9.18 - 2015-02-18
+* Copyright (c) 2015 ; Licensed  */
 'use strict';
 
 var fc = angular.module('ngFontChart', ['ngOdometer']);
@@ -20,10 +20,6 @@ fc.directive('ngFontChart', function() {
 		link: function(scope, element) {
 			angular.element(element.children()[0]).css('background-position','0px '+scope.value+'%');
 			angular.element(element.children()[0]).css('background-image', 'linear-gradient(to top, '+scope.startColor+' 0px, '+scope.startColor+' 50%, '+scope.endColor+' 50%, '+scope.endColor+' 100% )');
-
-			angular.element(element.children()[1]).css('background', scope.endColor);
-			angular.element(element.children()[1]).css('color', '#ffffff');
-
 			scope.$watch('value', function(value) {
 				if ( value != null ) {
 					angular.element(element.children()[0]).css('background-position','0px '+value+'%');
