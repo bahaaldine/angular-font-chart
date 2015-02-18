0bce84b6a3326468114d4859f13b1d5d12ab82dd
Bahaaldine Azarmi <bahaaldine@gmail.com>
2015-02-18 10:44:45 +0100

    fixed issue with templating

--- dist/angular-font-chart.js
+++ dist/angular-font-chart.js
@@ -1,4 +1,4 @@
-/*! angular-font-chart - v0.9.19 - 2015-02-18
+/*! angular-font-chart - v0.9.21 - 2015-02-18
 * Copyright (c) 2015 ; Licensed  */
 'use strict';
 
@@ -17,7 +17,7 @@ fc.directive('ngFontChart', function() {
 			extraClass: '&',
 			symbol: '@'
 		},
-		template: '<div><div class="ngfontchart icon background-transition-slow {{font}}"></div><ng-odometer class="value" value="value"></ng-odometer></div><span class="ng-odometer-symbol">{{symbol}}</span>',
+		template: '<div><div class="ngfontchart icon background-transition-slow {{font}}"></div><ng-odometer class="value" value="value"></ng-odometer><span class="ng-odometer-symbol">{{symbol}}</span></div>',
 		link: function(scope, element) {
 			angular.element(element.children()[0]).css('background-position','0px '+scope.value+'%');
 			angular.element(element.children()[0]).css('background-image', 'linear-gradient(to top, '+scope.startColor+' 0px, '+scope.startColor+' 50%, '+scope.endColor+' 50%, '+scope.endColor+' 100% )');
